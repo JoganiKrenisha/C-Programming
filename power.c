3b19841c93daadccfa29189e58275ec83f330cac
Jogani Krenisha L <25dcs042@charusat.edu.in>
2026-06-05 18:50:30 +0530

    C Program for power calculation

---
+++ power.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+void main()
+{
+    int a,b,power=1;
+    printf("Enter base :");
+    scanf("%d",&a);
+    printf("Enter power:");
+    scanf("%d",&b);
+    for(int i=1; i<=b; i++)
+    {
+        power=power*a;
+    }
+    printf("%d raised to the power %d is %d",a,b,power);
+}
