77afe950ab4fa1acc07b9372576acd305a00e564
Jogani Krenisha L <25dcs042@charusat.edu.in>
2026-06-05 18:46:12 +0530

    Matrix

---
+++ matrix.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int r,c;
+    printf("Enter the no. of raws:");
+    scanf("%d",&r);
+    printf("Enter the no. of columns:");
+     scanf("%d",&c);
+      int arr[r][c];
+     for(int i=0; i<r; i++){
+         for(int j=0; j<c; j++){
+
+             scanf("%d",&arr[i][j]);
+         }
+     }
+     printf("\nMetrix is\n");
+
+      for(int i=0; i<r; i++){
+         for(int j=0; j<c; j++)
+           {
+             printf("%d ",arr[i][j]);
+           }
+            printf("\n");
+      }
+
+              return 0;
+     }
+
+
+
+
